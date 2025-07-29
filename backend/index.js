e87a9c1ec52771876a8181ac4d41c5639f402503
mdayan96386 <ayan96386@gmail.com>
2025-07-29 14:41:46 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,10 @@ import userRouter from "./routes/user.routes.js"
 
 
 const app=express()
-app.use(cors())
+app.use(cors({
+    origin:["https://virtualassistant-uzrn.onrender.com","http://localhost:5173"],
+    credentials:true
+}))
 const port=process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser())
